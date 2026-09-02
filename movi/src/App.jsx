283a9bb3dc13c9bf3e9954f3f3d29bff8350a48c
ar56745 <prithvisuresh2866@gmail.com>
2026-09-01 22:33:08 -0400

    commit

--- movi/src/App.jsx
+++ movi/src/App.jsx
@@ -4,11 +4,10 @@ import MovieCard from "./components/MovieCard";
 import { useState, useEffect } from 'react'
 import { useDebounce } from "react-use";
 import './App.css'
-import {updateSearchCount} from "./appwrite.js";
+import {updateSearchCount, getTrendingMovies} from "./appwrite.js";
 
-
-const API_BASE_URL = 'https://api.themoviedb.org/3/discover/movie'   //tmdb API
-const API_KEY = import.meta.env.TMDB_API_KEY
+const API_BASE_URL = 'https://api.themoviedb.org/3'   //tmdb API
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 const API_METHODS = {
   method : 'GET',
   headers: {
@@ -23,6 +22,7 @@ const App = () => {
   const [errorMsg, setErrorMsg]= useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setLoading] = useState(true);
+  const [trendingMovies, setTrendingMovies] = useState([]);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
   useDebounce( () => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);  //waits for the user to stop tying for 500 ms, used to prevent making too many API calls
@@ -36,21 +36,22 @@ const App = () => {
           ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`     // if a query exists
           : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;             // if it doesn't
 //try
-      const response = await fetch(API_BASE_URL, API_METHODS);
-
+      const response = await fetch(endpoint, API_METHODS);
       if(!response.ok) throw new Error('Failed to retrieve movie results.');
 
       const data = await response.json();
       console.log(data);
 
-      if(data.Response === 'False') {
-        setErrorMsg(data.Error || 'Failed to fetch movies');
-        setMovieList([]);
-        return;
-      }
+      // if(data.Response === 'False') {
+      //   setErrorMsg(data.Error || 'Failed to fetch movies');
+      //   setMovieList([]);
+      //   return;
+      // }
       setMovieList(data.results || []);
 
-      updateSearchCount();
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     }
     catch (error) {                                                                            //catch
       console.log(`Ran into an error while fetching movies: ${error}`);
@@ -64,20 +65,52 @@ const App = () => {
     fetchMovies(debouncedSearchTerm)
   }, [debouncedSearchTerm])
 
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
+
 
   return (
       <main>
-        <div className='pattern'/>
+        <div className="pattern"/>
 
-        <div className='wrapper'>
+        <div className="wrapper">
           <header>
-            <img src='./hero.png' alt='Hero Banner'/>
-            <h1> Find <span className='text-gradient'>Movies</span> You'll Enjoy Without The Hassle</h1>
+            <img src="./hero.png" alt="Hero Banner" />
+            <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
           </header>
 
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          {trendingMovies.length > 0 && (
+              <section className="trending">
+                <h2>Trending Movies</h2>
+
+                <ul>
+                  {trendingMovies.map((movie, index) => (
+                      <li key={movie.$id}>
+                        <p>{index + 1}</p>
+                        <img src={movie.poster_url} alt={movie.title} />
+                      </li>
+                  ))}
+                </ul>
+              </section>
+          )}
+
           <section className="all-movies">
             <h2>All Movies</h2>
+
             {isLoading ? (
                 <Spinner />
             ) : errorMsg ? (
@@ -90,11 +123,10 @@ const App = () => {
                 </ul>
             )}
           </section>
-
-          <h1 className='text-white'>{searchTerm}</h1>
         </div>
       </main>
   )
 }
+
 
 export default App
--- movi/src/appwrite.js
+++ movi/src/appwrite.js
@@ -1,36 +1,51 @@
-import {Client, Databases, ID, Query} from "appwrite";
-const PROJECT_ID = import.meta.env.APPWRITE_API_KEY
-const DATABASE_ID = import.meta.env.APPWRITE_DATABASE_ID
-const COLLECTION_ID =
+import {Client, TablesDB, ID, Query} from "appwrite";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID
+const TABLE_ID = "6a9278430027d34927ff"
 
 
 const client = new Client()
     .setEndpoint('https://fra.cloud.appwrite.io/v1')
     .setProject(PROJECT_ID)
 
-const database = new Databases(client);
+const database = new TablesDB(client);
 
 export const updateSearchCount = async (searchTerm, movie) => {
     //uses appwrite sdk to see if the searchTerm already exists in the db, if it does, update the count
     //else, create a new document with the searchTerm and count as 1
 
     try {
-        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-            Query.equal('searchTerm', searchTerm),
-        ])
+        const result = await database.listRows({
+            databaseId: DATABASE_ID,
+            tableId: TABLE_ID,
+            queries: [
+                Query.equal('searchTerm', searchTerm),
+            ]
+        })
 
-        if (result.documents.length > 0) {        // if exists in db
-            const doc = result.documents[0];
+        if (result.rows.length > 0) {        // if exists in db
+            const row = result.rows[0];
 
-            await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
-                count: doc.count + 1,
+            await database.updateRow({
+                databaseId: DATABASE_ID,
+                tableId: TABLE_ID,
+                rowId: row.$id,
+                data: {
+                    count: row.count + 1,
+                }
             })
         } else {                                  // else
-            await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
-                searchTerm,
-                count: 1,
-                movie_id: movie.id,
-                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            await database.createRow({
+                databaseId: DATABASE_ID,
+                tableId: TABLE_ID,
+                rowId: ID.unique(),
+                data: {
+                    searchTerm,
+                    count: 1,
+                    movie_id: movie.id,
+                    poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+                }
             })
         }
     }
@@ -41,13 +56,18 @@ export const updateSearchCount = async (searchTerm, movie) => {
 
 export const getTrendingMovies = async () => {
     try {
-        const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-            Query.limit(5),
-            Query.orderDesc("count")
-        ])
+        const result = await database.listRows({
+            databaseId: DATABASE_ID,
+            tableId: TABLE_ID,
+            queries: [
+                Query.limit(5),
+                Query.orderDesc("count")
+            ]
+        })
 
-        return result.documents;
+        return result.rows;
     } catch (error) {
         console.error(error);
+        return [];
     }
 }
